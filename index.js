2d7c45b6661f4aa12df61d8dc58d1b5b486fd1cf
Vasanthraj-vr31 <vasanthrajj2005@gmail.com>
2025-12-19 12:39:55 +0530

    Fix CORS configuration

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ const app = express()
 
 connectDb();
 
-app.use(cors())
+const corsOptions = {
+    origin: ['https://serviceride-client.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use('/api', route);
