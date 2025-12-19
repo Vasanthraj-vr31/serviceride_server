2405210a49a099afcd920714cd284ee448d8e2d0
Vasanthraj-vr31 <vasanthrajj2005@gmail.com>
2025-12-19 12:23:45 +0530

    finally

--- Db/db.js
+++ Db/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const connectDb = async () => {
    try {
-      await mongoose.connect(process.env.MONGODB_URL);
+      mongoose.connect(process.env.MONGODB_URL);
       console.log("Database connected successfully");
    } catch (err) {
       console.error("Database connection failed:", err);
